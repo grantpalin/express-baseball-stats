f106782ac8c260df004c056a10a9e8e2be4f78a4
Grant Palin <grant@grantpalin.com>
2017-11-05 16:17:01 -0800

    Moved team data to external JSON file

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+// read the JSON file to get the team data
+var fs = require('fs');
+var teamDataJson = JSON.parse(fs.readFileSync('data/teamData.json', 'utf8'));
+
+// extract the team data from the JSON
+var teamData = teamDataJson.teamData;
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express MLB Standings' });
+  res.render('index', { title: 'Express MLB Standings', teamData: teamData });
 });
 
 module.exports = router;
