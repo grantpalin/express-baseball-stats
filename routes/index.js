2e39441d246cb0cdb70d4a3108e039b17650e221
Grant Palin <grant@grantpalin.com>
2017-11-12 11:18:46 -0800

    Added calculation and display of win/loss percentages

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,18 @@ var teamDataJson = JSON.parse(fs.readFileSync('data/teamData.json', 'utf8'));
 // extract the team data from the JSON
 var teamData = teamDataJson.teamData;
 
+// helper function for win/loss percentage calculations
+function pct(win, lose) {
+    return (win / (win + lose)).toFixed(3).slice(1);
+}
+
+// calculate the win/loss percentages - total, home, and away
+teamData.forEach(function cb(currentValue, index, array) {
+    currentValue.pct     = pct(currentValue.win,     currentValue.lose);
+    currentValue.pctHome = pct(currentValue.winHome, currentValue.loseHome);
+    currentValue.pctAway = pct(currentValue.winAway, currentValue.loseAway);
+});
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express MLB Standings', teamData: teamData });
 });
